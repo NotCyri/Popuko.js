b5265bee6285706feb1adbfdf63da7e125f6a4d8
Seeth <50422521+seeth3r@users.noreply.github.com>
2019-05-19 13:35:57 -0700

    Add files via upload

---
+++ package.js
@@ -0,0 +1,35 @@
+const cp = require('child_process')
+
+const req = async module => {
+  try {
+    require.resolve(module)
+  } catch (e) {
+    console.log(`Could not resolve "${module}"\nInstalling`)
+    cp.execSync(`npm install ${module}`)
+    await setImmediate(() => {})
+    console.log(`"${module}" has been installed`)
+  }
+  console.log(`Requiring "${module}"`)
+  try {
+    return require(module)
+  } catch (e) {
+    console.log(`Could not include "${module}". Restart the script`)
+    process.exit(1)
+  }
+}
+
+const main = async () => {
+  const request     = await req('request')
+  const ms      = await req('ms')
+  const fs      = await req('fs')
+  const mathjs  = await req('mathjs')
+  const figlet  = await req('figlet')
+  const snekfetch   = await req('snekfetch')
+  const customtranslate     = await req('custom-translate')
+  const weatherjs    = await req('weather-js')
+  const google    = await req('google')
+
+  // The rest of the app's code goes here
+}
+
+main()
